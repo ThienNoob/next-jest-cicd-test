83224d897710015d1a5260f579de3bb60d398eea
ThienNoob <102866314+ThienNoob@users.noreply.github.com>
2024-05-03 00:19:52 +0700

    hahah

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,10 +8,15 @@ export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
+export async function generateStaticParams() {
+  const posts = await fetch('https://.../posts').then((res) => res.json())
+ 
+  return posts.map((post: { slug: any; }) => ({
+    slug: post.slug,
+  }))
+}
+
 export default function Page({ params }: Params) {
-  return <>
-  <h1>Slug: {params.slug}</h1>;
-  <p>ChucThien</p>
-  <p>Welcome</p>
-  </>
+  const { slug } = params
+  return <h1>Slug: {params.slug}</h1>;
 }
